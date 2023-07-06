19be8e271fccd7faf9b10e9932cd36575103a096
beratridvanasilturk <br.asltrk@gmail.com>
2023-07-06 15:51:03 +0300

    Split View Created

--- Split View/TableViewController.swift
+++ Split View/TableViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class TableViewController: UITableViewController {
 
+    //MARK: Variables
+    var dataArray = Array<String>()
+    
+    //MARK: Functions
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,29 +21,45 @@ class TableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        setInitViews()
     }
 
     // MARK: - Table view data source
+    
+    private func setInitViews(){
+        
+        for i in 1..<11{
+            dataArray.append("Row \(i)")
+        }
+        
+    }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return dataArray.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath) as! TableViewCell
+        cell.cellTitleLabel.text = dataArray[indexPath.row]
+        
         return cell
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(identifier: "ViewController") as! ViewController
+        vc.text = "\(dataArray[indexPath.row]) Data"
+        
+        splitViewController?.showDetailViewController(vc, sender: nil)
+    }
 
     /*
     // Override to support conditional editing of the table view.
